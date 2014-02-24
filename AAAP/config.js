941ace2f30b4880e963c301059ca8449f9d11f1e
PaulGreco <paul_greco@yahoo.com>
2014-02-24 15:41:51 -0600

    Adding all the modules that github did not want to add before.

---
+++ AAAP/config.js
@@ -0,0 +1,29 @@
+var RequestPort = 80;
+var RequestEPort = 4434;
+
+var SQLhost = 'localhost';
+var SQLdatabase = 'aaapdata';
+var SQLport = '3306';
+var SQLuser = 'paul';
+var SQLpassword = 'asdfghjkl';
+
+var authAAGroupHandlerKey = "/authAAGroup";
+var authUserHandlerKey = "/authUser";
+var postHandlerKey = "/post";
+var getHandlerKey = "/get";
+var invalidRequestHandlerKey = "invalidRequest";
+
+exports.RequestPort = RequestPort;
+exports.RequestEPort = RequestEPort;
+
+exports.SQLhost = SQLhost;
+exports.SQLdatabase = SQLdatabase;
+exports.SQLport = SQLport;
+exports.SQLuser = SQLuser;
+exports.SQLpassword = SQLpassword;
+
+exports.authAAGroupHandlerKey = authAAGroupHandlerKey;
+exports.authUserHandlerKey = authUserHandlerKey;
+exports.postHandlerKey = postHandlerKey;
+exports.getHandlerKey = getHandlerKey;
+exports.invalidRequestHandlerKey = invalidRequestHandlerKey;
